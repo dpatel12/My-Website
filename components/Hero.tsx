12fc4fcf869fb51a40da118c36f095b26f97395f
dpatel12 <danyaalp99@gmail.com>
2024-08-16 00:38:18 -0700

    Hero file

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,11 @@
-
+'use client';
 import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import React from 'react';
+import Typewriter from './Typewriter';
+import MagicButton from "./ui/MagicButton";
 
-function Hero() {
+const Hero = () => {
   return (
     <div className="pb-20 pt-36"> 
         <div>
@@ -10,13 +14,56 @@ function Hero() {
             fill="white"
             />
             <Spotlight
-            className="h-[80vh] w-[50vw] top-10 left-full"
-            fill="green"
+            className="top-10 left-full h-[80vh] w-[50vw]" fill="green"
             />
-            <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+            <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
+        </div>
+        <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black/[0.2]
+        flex items-center justify-center absolute top-0 left-0">
+          <div
+            className="absolute pointer-events-none inset-0 flex items-center justify-center mask-image:radial-gradient(ellipse_at_center, transparent 20%, black)">
+          </div>
+        </div>
+        <div className="flex justify-center relative my-20 z-10"> 
+          <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
+            <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+              Dynamic Web Magic with Next.js
+            </h2>
+            <h2>
+              <TextGenerateEffect
+                className="text-center text-[40px] md:text-5xl lg:text-6xl"
+                words="Hi, I'm Danyaal Patel, a recent Computer Science & Business Graduate"
+              /> 
+            </h2>
+            <div className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+                <Typewriter
+                    initialText="I like to "
+                    words={[
+                        'learn new things.',
+                        'fuse tech know-how with business savvy.',
+                        'create full-stack projects.',
+                        'play, watch, breathe 🏈.',
+                        'master the art of Agile project management',
+                        'streamline everyday tasks with automation.',
+                        'dive into data and see what it tells me',
+                        'bridge the gap between tech and business goals'
+                    ]}
+                    typingSpeed={75}
+                    deletingSpeed={40}
+                    delayBetweenPhrases={550}
+                    initialDelay={2000}
+                />
+
+            </div>
+            <a href="#about">
+                <MagicButton
+                  title = "Show my work"
+                />
+            </a>
+          </div>
         </div>
     </div>
-  )
+  );
 }
 
 export default Hero
