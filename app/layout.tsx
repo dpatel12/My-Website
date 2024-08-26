6dc40518c215922e9e277abecf097afa8d7a2575
dpatel12 <danyaalp99@gmail.com>
2024-08-26 15:20:18 -0700

    google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,11 @@ import "./globals.css";
 import { ThemeProvider } from "./provider";
 import Head from 'next/head';
 
+declare global {
+  interface Window {
+    dataLayer: any;
+  }
+}
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +29,17 @@ export default function RootLayout({
     <html lang="en">
       <Head>
         <link rel="icon" href="/favicon.ico" sizes="any"/>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-PQ6FN6YXQR"></script>
+          <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-PQ6FN6YXQR');
+            `,
+          }}
+        />
       </Head>
       <body className={inter.className}>
         <ThemeProvider
