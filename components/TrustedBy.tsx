b65943e81f55aef02fe82956cf0ae14e39adeff8
dpatel12 <danyaalp99@gmail.com>
2024-09-02 00:23:35 -0700

    fixing size

--- components/TrustedBy.tsx
+++ components/TrustedBy.tsx
@@ -6,13 +6,13 @@ import { InfiniteMovingCards } from "./ui/InfiniteCards";
 
 const TrustedBy = () => {
   return (
-    <section id="trusted-by" className="py-20">
+    <section id="trusted-by" className="p">
       <h1 className="heading">
         Trusted by
         <span className="text-purple"> leading companies</span>
       </h1>
 
-      <div className="flex flex-col items-center max-lg:mt-10">
+      <div className="flex flex-col items-center max-lg:mt-5">
         <div
           className="h-[50vh] md:h-[30rem] rounded-md flex flex-col antialiased items-center justify-center relative overflow-hidden"
         >
